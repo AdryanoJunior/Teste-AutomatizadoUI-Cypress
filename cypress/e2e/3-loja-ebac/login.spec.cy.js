df0a96abdb66a3e43be0dfc5a54b0857c9f276d5
Adryano Junior <jusamsung10@gmail.com>
2024-04-23 14:18:55 -0300

    testes negativos e hooks

--- cypress/e2e/3-loja-ebac/login.spec.cy.js
+++ cypress/e2e/3-loja-ebac/login.spec.cy.js
@@ -2,15 +2,39 @@
 
 describe('Funcionalidade: Login', () => {
 
+    beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')   
+    });
+
+    afterEach(() => {
+        cy.screenshot()
+    });
+
 it('Deve fazer login com sucesso', () => {
-cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
 cy.get('#username').type('adryanoqaenginner@gmail.com')
 cy.get('#password').type('Junior78$')
 cy.get('.woocommerce-form > .button').click()
-
 cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, adryanoqaenginner (não é adryanoqaenginner? Sair)' )
-cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain' , 'A partir do painel de controle de sua conta, você pode ver suas compras recentes, gerenciar seus endereços de entrega e faturamento, e editar sua senha e detalhes da conta.' )
+
+});
+
+it('Deve exibir uma mensagem de erro ao inserir usuário inválido', () => {
+cy.get('#username').type('junin8qaenginner@gmail.com')
+cy.get('#password').type('Junior78$')
+cy.get('.woocommerce-form > .button').click()
+cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+cy.get('.woocommerce-error').should('exist')
+
+});
+
+it('Deve exibir uma mensagem de erro ao inserir senha inválida', () => {
+cy.get('#username').type('adryanoqaenginner@gmail.com')
+cy.get('#password').type('Junin78$')
+cy.get('.woocommerce-form > .button').click()
+cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail adryanoqaenginner@gmail.com está incorreta. Perdeu a senha?' )
+cy.get('.woocommerce-error').should('exist')
+
+});
 
 })
     
-})
