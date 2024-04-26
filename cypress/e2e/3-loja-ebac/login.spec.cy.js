6705801596c4fd6153c2d1dfefb5d17c078c3ed8
Adryano Junior <jusamsung10@gmail.com>
2024-04-26 18:33:51 -0300

    login usando massa de dados

--- cypress/e2e/3-loja-ebac/login.spec.cy.js
+++ cypress/e2e/3-loja-ebac/login.spec.cy.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress"/>
+const perfil = require('../../fixtures/perfil.json')
 
 describe('Funcionalidade: Login', () => {
 
     beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')   
+    cy.visit('minha-conta')   
     });
 
     afterEach(() => {
@@ -33,6 +34,23 @@ cy.get('#password').type('Junin78$')
 cy.get('.woocommerce-form > .button').click()
 cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail adryanoqaenginner@gmail.com está incorreta. Perdeu a senha?' )
 cy.get('.woocommerce-error').should('exist')
+
+});
+
+it('Deve fazer login com sucesso - Usando massa de dados', () => {
+cy.get('#username').type(perfil.usuário)
+cy.get('#password').type(perfil.senha)
+cy.get('.woocommerce-form > .button').click()
+cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, adryanoqaenginner (não é adryanoqaenginner? Sair)' )
+});
+
+it('Deve fazer login com sucesso - Usando Fixture ', () => {
+cy.fixture('perfil').then( dados => {
+    cy.get('#username').type(dados.usuário), {log: false}
+    cy.get('#password').type(dados.senha , {log: false})
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, adryanoqaenginner (não é adryanoqaenginner? Sair)' )    
+})
 
 });
 
--- cypress/e2e/3-loja-ebac/produtos.spec.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.spec.cy.js
@@ -3,7 +3,7 @@
 describe('Funcionalidade: Produtos', () => {
 
 beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    cy.visit('produtos')
 
 });
 
@@ -12,16 +12,16 @@ beforeEach(() => {
         //.first()
         //.last()
         //.eq(8)
-        .contains('Ariel Roll Sleeve Sweatshirt')
+        .contains('Aero Daily Fitness Tee')
         .click()
 
         cy.get('#tab-title-additional_information > a').should('contain', 'Informação adicional')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
         cy.get('.button-variable-item-M').click()
-        cy.get('.button-variable-item-Red').click()
+        cy.get('.button-variable-item-Yellow').click()
         cy.get('.single_add_to_cart_button').click()
 
-        cy.get('.woocommerce-message').should('contain', '“Ariel Roll Sleeve Sweatshirt” foi adicionado no seu carrinho.')
+        cy.get('.woocommerce-message').should('contain', '“Aero Daily Fitness Tee” foi adicionado no seu carrinho.')
         cy.get('.woocommerce-message > .button').click()
         cy.get('.checkout-button').click()
 
